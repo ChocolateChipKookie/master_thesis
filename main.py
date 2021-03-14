400c4b7f8be7311ec04699f5e4ef7dee5af272da
ChocolateChipKookie <adi.causevic7@gmail.com>
2021-03-14 20:47:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import util
+
+dataset = util.ImageDataset("/media/kookie/DE9AF7459AF718AB/Pictures")
+
+print(len(dataset))
+
---
+++ util.py
@@ -0,0 +1,41 @@
+from torch.utils.data import Dataset
+import os
+from typing import Union, List, Tuple, Callable
+
+
+class ImageDataset(Dataset):
+    def __init__(self, path: str, recursive: bool = True, suffixes:Union[Tuple[str], str] = None):
+        self.item_paths = []
+
+        # Set check if extension is valid
+        if suffixes and isinstance(suffixes, str):
+            suffixes = (suffixes, )
+            check_ext: Callable[[str], bool] = lambda x: x.lower() in suffixes
+        else:
+            check_ext: Callable[[str], bool] = lambda x: True
+
+        def walk(current_path):
+            # Check all files in directory
+            for file in os.listdir(current_path):
+                # Create filepath
+                filepath = os.path.join(current_path, file)
+                # Check if it is a file
+                if os.path.isfile(filepath):
+                    # Fetch extension
+                    ext = os.path.splitext(file)[1]
+                    # If the extension is valid add path to list
+                    if check_ext(ext):
+                        # Add to file paths
+                        self.item_paths.append(filepath)
+                # Check if it is a directory
+                elif recursive and os.path.isdir(filepath):
+                    # If it is a directory, walk the directory
+                    walk(filepath)
+
+        walk(path)
+
+    def __len__(self):
+        return len(self.item_paths)
+
+    def __getitem__(self, index):
+        return None
