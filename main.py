41f72e1c498947d71a3ea9aa3804d0fae6ca9003
ChocolateChipKookie <adi.causevic7@gmail.com>
2021-03-15 20:52:42 +0100

    Test

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import util
 import os
 import torchvision
 
+
 #dataset = util.ImageDataset("/hdd/imagenet/train")
 #print(len(dataset))
 
-dataset = torchvision.datasets.ImageNet("/hdd/imagenet", "train")
+dataset = torchvision.datasets.ImageFolder("/hdd/imagenet/train")
 
-print(dataset)
+print(len(dataset))
