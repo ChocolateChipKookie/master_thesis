950db841f051b80b62083bccc52bcfa6541e97d0
ChocolateChipKookie <adi.causevic7@gmail.com>
2021-06-15 16:52:29 +0200

    Revert

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import torch.cuda
 from util import util
 
 if __name__ == "__main__":
-    torch.cuda.set_per_process_memory_fraction(0.99, torch.device("cuda:0"))
     # Create parser
     parser = argparse.ArgumentParser(description="Dispatcher for training colorization models")
     parser.add_argument('config', metavar='config', type=str, help="Config file path")
